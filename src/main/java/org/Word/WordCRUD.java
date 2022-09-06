6983b45f75abb2df53e0d1a83ba43ea10c2c5337
yeseul <yeseul0900@handong.ac.kr>
2022-09-07 00:09:34 +0900

    4번 구현

--- src/main/java/org/Word/WordCRUD.java
+++ src/main/java/org/Word/WordCRUD.java
@@ -1,10 +1,32 @@
 package org.Word;
 
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
     @Override
     public Object add() {
-        return null;
+        //입력 구현
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.next();
+        s.next();
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0,level,word,meaning);
+    }
+
+    public void addWord() {
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다 !!!");
     }
 
     @Override
--- src/main/java/org/Word/WordManager.java
+++ src/main/java/org/Word/WordManager.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class WordManager {
     Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
     public int selectMenu(){
         System.out.print("*** 영단어 마스터 ***\n" +
                 "********************\n" +
@@ -19,11 +23,13 @@ public class WordManager {
                 "=> 원하는 메뉴는? ");
         return s.nextInt();
     }
+
     public void start(){
         while(true){
             int menu = selectMenu();
             if(menu == 0) break;
             if(menu == 4){
+                wordCRUD.addWord();
                 //create
             }
             if(menu == 1){
