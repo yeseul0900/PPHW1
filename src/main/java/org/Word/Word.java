4edb9b90189d3cb0f152af68e27b5e302d123ea5
yeseul <yeseul0900@handong.ac.kr>
2022-09-06 23:19:04 +0900

    make interface

---
+++ src/main/java/org/Word/Word.java
@@ -0,0 +1,53 @@
+package org.Word;
+
+public class Word {
+    //멤버변수
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+    //생성자
+
+
+    public Word() {}
+
+    public Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    //게터세터
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
