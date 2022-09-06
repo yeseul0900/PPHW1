91086be34addf0b104aa0dddfe6b926471bdae37
yeseul <yeseul0900@handong.ac.kr>
2022-09-06 23:21:59 +0900

    make WordCRUD

---
+++ src/main/java/org/Word/WordCRUD.java
@@ -0,0 +1,24 @@
+package org.Word;
+
+public class WordCRUD implements ICRUD{
+
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+}
