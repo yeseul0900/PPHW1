991560a6cd2e3c3827969bd2b05679e23b936c65
yeseul <yeseul0900@handong.ac.kr>
2022-09-06 23:55:38 +0900

    틀을 만듬

--- src/main/java/org/Word/Main.java
+++ src/main/java/org/Word/Main.java
@@ -1,7 +1,7 @@
 package org.Word;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        new WordManager().start();
     }
 }
