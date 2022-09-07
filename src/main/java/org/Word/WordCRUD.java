70d8bea28f7d46cbdbf082a85f8369f943e1131f
yeseul <yeseul0900@handong.ac.kr>
2022-09-07 19:43:31 +0900

    1번 4번 구현

--- src/main/java/org/Word/WordCRUD.java
+++ src/main/java/org/Word/WordCRUD.java
@@ -13,11 +13,12 @@ public class WordCRUD implements ICRUD{
     @Override
     public Object add() {
         //입력 구현
-        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.print("\n=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();
         System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();
+        System.out.println();
 
         return new Word(0,level,word,meaning);
     }
@@ -25,7 +26,7 @@ public class WordCRUD implements ICRUD{
     public void addWord() {
         Word one = (Word)add();
         list.add(one);
-        System.out.println("새 단어가 단어장에 추가되었습니다 !!!");
+        System.out.println("새 단어가 단어장에 추가되었습니다 !!!\n");
     }
 
     @Override
@@ -43,11 +44,11 @@ public class WordCRUD implements ICRUD{
 
     }
     public void listAll(){
-        System.out.println("--------------------------------");
+        System.out.println("\n--------------------------------");
         for(int i = 0; i<list.size(); i++){
             System.out.print((i+1)+" ");
             System.out.println(list.get(i).toString());
         }
-        System.out.println("--------------------------------");
+        System.out.println("--------------------------------\n");
     }
 }
