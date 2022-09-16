a6f1cd87284d3162d8f498ea61b28063296bf413
yeseul <yeseul0900@handong.ac.kr>
2022-09-16 23:58:48 +0900

    파일추가 기능 완료

--- src/main/java/org/Word/WordCRUD.java
+++ src/main/java/org/Word/WordCRUD.java
@@ -102,19 +102,26 @@ public class WordCRUD implements ICRUD{
         System.out.println("--------------------------------\n");
         return idlist;
     }
-    public void loadFile() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fname));
-        String line;
-        while(true){
-            line = br.readLine();
-            if(line == null) break;
-            String data[] = line.split("\\|");
-            int level = Integer.parseInt(data[0]);
-            String word = data[1];
-            String meaning = data[2];
-            list.add(new Word(0, level, word, meaning));
+    public void loadFile(){
+        int count;
+        try (BufferedReader br = new BufferedReader(new FileReader(fname))) {
+            String line;
+            count = 0;
+            while (true) {
+                line = br.readLine();
+                if (line == null) break;
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        br.close();
+        System.out.println("===>" + count + "개 로딩 완료!!!");
 
     }
 
